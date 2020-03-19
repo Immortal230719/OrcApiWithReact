b3b693631effbd2f1e3ecdc8dd48c577e22b142a
Immortal230719 <golub010919@mail.ru>
2020-03-19 17:57:07 +0400

    singleProduct page created

--- src/containers/SingleProduct/index.js
+++ src/containers/SingleProduct/index.js
@@ -1,46 +1,129 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Typography,
+  List,
+  ListItem,
+  ListItemIcon,
+  Avatar,
+  ListItemText,
+  ListSubheader,
+  Button
+} from "@material-ui/core";
+import { getProduct } from "selectors";
+import { fetchSingleProduct } from "actions/apiActions";
+import { connect } from "react-redux";
 
-import  {makeStyles} from '@material-ui/core/styles';
-
-import { getProduct } from 'selectors';
-import { fetchSingleProduct } from 'actions/apiActions';
-import { connect } from 'react-redux';
+import Layout from "components/Layout";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles({
   wrapper: {
-    color: 'red'
+    textDecoration: "none",
+    color: "#acacac"
+  },
+  text: {
+    width: "50%",
+    margin: "0 auto"
+  },
+  subTitle: {
+    fontSize: "25px"
+  },
+  btnBack: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "60px",
+    width: "50%",
+    margin: "0 auto",
+    marginTop: "30px",
+    cursor: "pointer",
+    textTransform: "uppercase",
+    background:
+      "linear-gradient(160deg, rgb(44, 206, 255), rgb(10, 98, 199) 100%)",
+    borderRadius: "8px",
+    color: "#fff",
+    textDecoration: "none"
   }
-})
+});
 
-const SingleProduct = ({product, fetchSingleProduct, match}) => {
-  const slug = match.params.slug
+const SingleProduct = ({ product, fetchSingleProduct, match }) => {
+  const slugInRouter = match.params.slug;
+  const styles = useStyles();
+  const { title, description, owners } = product;
 
-  useEffect(() => {        
-    fetchSingleProduct(slug)
-  }, [fetchSingleProduct, slug])
+  const renderOwners = (owners, styles) => {
+    if (Array.isArray(owners)) {
+      return owners.map(({ id, name, email, avatar }) => {
+        return (
+          <Link className={styles.wrapper} key={email} to="/owners/">
+            <ListItem button>
+              <ListItemIcon>
+                <Avatar variant="rounded" src={avatar} />
+              </ListItemIcon>
+              <ListItemText primary={name} />
+              <ListItemText primary={email} />
+            </ListItem>
+          </Link>
+        );
+      });
+    } else return null;
+  };
 
-  console.log(product);
-  
+  useEffect(() => {
+    fetchSingleProduct(slugInRouter);
+  }, [fetchSingleProduct, slugInRouter]);
 
-  const styles = useStyles()
-
-  return(
-    <div
-      className={styles.wrapper}
-    >
-      product
-    </div>
-  )
-}
+  return (
+    <Layout>
+      <Typography
+        align="center"
+        variant="h3"
+        component="h1"
+        gutterBottom={true}
+      >
+        {title}
+      </Typography>
+      <Typography
+        className={styles.text}
+        paragraph={true}
+        align="center"
+        variant="body2"
+        component="p"
+      >
+        {description}
+      </Typography>
+      <List
+        aria-labelledby="nested-list-subheader"
+        subheader={
+          <ListSubheader
+            className={styles.subTitle}
+            component="h2"
+            id="nested-list-subheader"
+          >
+            Owners
+          </ListSubheader>
+        }
+      >
+        {renderOwners(owners, styles)}
+        <Link className={styles.wrapper} to="/">
+          <Button className={styles.btnBack} variant="contained">
+            Back to Products
+          </Button>
+        </Link>
+      </List>
+    </Layout>
+  );
+};
 
 const mapStateToProps = state => {
   return {
     product: getProduct(state)
-  }
-}
+  };
+};
 
 const mapDispatchToProps = {
   fetchSingleProduct
-}
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(SingleProduct);
